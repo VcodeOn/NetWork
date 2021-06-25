34a75ba07eed114473cfb548a943de0a27073dee
zcmaye <zcmaye@qq.com>
2021-06-26 02:40:12 +0800

    remove my username

--- CSmtp/main.c
+++ CSmtp/main.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-	initSmtp("842831611@qq.com", "sthhxznovshqbbeg", "825943234@qq.com");
+	initSmtp("xxx@qq.com", "xxx", "xxx@qq.com");
 
 	setPortDomain(25, "smtp.qq.com");
 	setMailContent("hello", "你好我是邮件正文");
